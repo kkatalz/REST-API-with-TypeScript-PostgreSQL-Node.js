8013f7bf7113755c5a7b6c323cb5abc6d340256b
kkatalz <zlata.kiev2004@gmail.com>
2025-08-15 14:12:43 +0300

    Implement validation for username, email and password.

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,18 @@
 import { CreateUserDto } from '@/user/dto/createUser.dto';
 import { UserService } from '@/user/user.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UserControlller {
   constructor(private readonly userService: UserService) {}
 
+  @UsePipes(new ValidationPipe())
   @Post()
   async createUser(@Body('user') createUserDto: CreateUserDto): Promise<any> {
     return await this.userService.createUser(createUserDto);
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,7 @@
 import { CreateUserDto } from '@/user/dto/createUser.dto';
 import { IUserResponse } from '@/user/types/userResponse.interface';
 import { UserEntity } from '@/user/user.entity';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { sign, verify } from 'jsonwebtoken';
@@ -19,6 +19,25 @@ export class UserService {
   async createUser(createUserDto: CreateUserDto): Promise<IUserResponse> {
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
+
+    const userByEmail = await this.userRepository.findOne({
+      where: {
+        email: createUserDto.email,
+      },
+    });
+
+    const userByUsername = await this.userRepository.findOne({
+      where: {
+        username: createUserDto.username,
+      },
+    });
+
+    if (userByEmail || userByUsername) {
+      throw new HttpException(
+        'Email or username is already taken',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
 
     const savedUser = await this.userRepository.save(newUser);
     return this.generateUserResponse(savedUser);
