ef7b39685477724710594394feb905bd58288ab4
kkatalz <zlata.kiev2004@gmail.com>
2025-08-15 13:56:15 +0300

    Add generation jwt token.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,15 @@ import { TagModule } from './tag/tag.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import ormconfig from './ormconfig';
 import { UserModule } from '@/user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule],
+  imports: [
+    TypeOrmModule.forRoot(ormconfig),
+    TagModule,
+    UserModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,13 @@
 import { CreateUserDto } from '@/user/dto/createUser.dto';
+import { IUserResponse } from '@/user/types/userResponse.interface';
 import { UserEntity } from '@/user/user.entity';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { sign, verify } from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Injectable()
 export class UserService {
@@ -11,10 +16,31 @@ export class UserService {
     private readonly userRepository: Repository<UserEntity>,
   ) {}
 
-  async createUser(createUserDto: CreateUserDto): Promise<CreateUserDto> {
+  async createUser(createUserDto: CreateUserDto): Promise<IUserResponse> {
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
 
-    return await this.userRepository.save(newUser);
+    const savedUser = await this.userRepository.save(newUser);
+    return this.generateUserResponse(savedUser);
+  }
+
+  generateToken(user: UserEntity): string {
+    return sign(
+      {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+      process.env.JWT_SECRET,
+    );
+
+    // const decode = verify(generatedToken, 'SECRET9090');
+    // console.log(decode);
+  }
+
+  generateUserResponse(user: UserEntity): IUserResponse {
+    return {
+      user: { ...user, token: this.generateToken(user) },
+    };
   }
 }
