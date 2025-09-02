9cfc5dc44c79cc2a285bf26f73086741ff3fa78b
kkatalz <zlata.kiev2004@gmail.com>
2025-09-02 14:52:47 +0300

    Fix error display on 'user is taken'.

--- blog-frontend/src/pages/register.tsx
+++ blog-frontend/src/pages/register.tsx
@@ -1,29 +1,30 @@
-import { useMutation } from "@tanstack/react-query"
-import { AxiosError } from "axios"
-import { Field, Form, Formik } from "formik"
-import { NavLink, useNavigate } from "react-router-dom"
-import { toFormikValidationSchema } from "zod-formik-adapter"
-import { register } from "../services/auth.service"
-import { useAuthStore } from "../shared/data-access/store/auth.store"
-import { RegisterValidationSchema } from "../shared/data-access/zod-schema"
-import { ErrorList } from "../shared/ui/error-list"
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { Field, Form, Formik } from "formik";
+import { NavLink, useNavigate } from "react-router-dom";
+import { toFormikValidationSchema } from "zod-formik-adapter";
+import { register } from "../services/auth.service";
+import { useAuthStore } from "../shared/data-access/store/auth.store";
+import { RegisterValidationSchema } from "../shared/data-access/zod-schema";
+import { ErrorList } from "../shared/ui/error-list";
+import { Error } from "../shared/ui/error";
 
 const initialValues = {
   username: "",
   email: "",
-  password: ""
-}
+  password: "",
+};
 
 export const Register = () => {
-  const navigate = useNavigate()
-  const { setAuthState } = useAuthStore()
+  const navigate = useNavigate();
+  const { setAuthState } = useAuthStore();
   const { isPending, mutate, isError, error } = useMutation({
     mutationFn: register,
     onSuccess: (data) => {
-      setAuthState(data.user, true)
-      navigate("/")
-    }
-  })
+      setAuthState(data.user, true);
+      navigate("/");
+    },
+  });
 
   return (
     <div className="auth-page">
@@ -35,37 +36,59 @@ export const Register = () => {
               <NavLink to="/login">Have an account?</NavLink>
             </p>
 
-            {isError && <ErrorList errors={((error as AxiosError).response?.data as any)?.errors} />}
+            {isError && <Error error={(error as AxiosError).message} />}
 
             <Formik
               initialValues={initialValues}
-              validationSchema={toFormikValidationSchema(RegisterValidationSchema)}
+              validationSchema={toFormikValidationSchema(
+                RegisterValidationSchema
+              )}
               onSubmit={(user) => mutate({ user })}
             >
-              {
-                ({ errors, touched }) => (
-                  <Form>
-                    <fieldset className="form-group" disabled={isPending}>
-                      <Field name="username" className="form-control form-control-lg" type="text" placeholder="Username" />
-                      {errors.username && touched.username ? <span>{errors.username}</span> : null}
-                    </fieldset>
-                    <fieldset className="form-group" disabled={isPending}>
-                      <Field name="email" className="form-control form-control-lg" type="text" placeholder="Email" />
-                      {errors.email && touched.email ? <span>{errors.email}</span> : null}
-                    </fieldset>
-                    <fieldset className="form-group" disabled={isPending}>
-                      <Field name="password" className="form-control form-control-lg" type="password" placeholder="Password" />
-                      {errors.password && touched.password ? <span>{errors.password}</span> : null}
-                    </fieldset>
-                    <button className="btn btn-lg btn-primary pull-xs-right">Sign up</button>
-                  </Form>
-                )
-              }
+              {({ errors, touched }) => (
+                <Form>
+                  <fieldset className="form-group" disabled={isPending}>
+                    <Field
+                      name="username"
+                      className="form-control form-control-lg"
+                      type="text"
+                      placeholder="Username"
+                    />
+                    {errors.username && touched.username ? (
+                      <span>{errors.username}</span>
+                    ) : null}
+                  </fieldset>
+                  <fieldset className="form-group" disabled={isPending}>
+                    <Field
+                      name="email"
+                      className="form-control form-control-lg"
+                      type="text"
+                      placeholder="Email"
+                    />
+                    {errors.email && touched.email ? (
+                      <span>{errors.email}</span>
+                    ) : null}
+                  </fieldset>
+                  <fieldset className="form-group" disabled={isPending}>
+                    <Field
+                      name="password"
+                      className="form-control form-control-lg"
+                      type="password"
+                      placeholder="Password"
+                    />
+                    {errors.password && touched.password ? (
+                      <span>{errors.password}</span>
+                    ) : null}
+                  </fieldset>
+                  <button className="btn btn-lg btn-primary pull-xs-right">
+                    Sign up
+                  </button>
+                </Form>
+              )}
             </Formik>
-
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
