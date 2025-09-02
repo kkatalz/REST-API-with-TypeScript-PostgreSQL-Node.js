fe6372aeb9cc85347727f490f0ab07eb33cc4ca5
kkatalz <zlata.kiev2004@gmail.com>
2025-09-02 14:46:54 +0300

    Fix correct error message display on incorrect email/password.

--- blog-frontend/src/pages/login.tsx
+++ blog-frontend/src/pages/login.tsx
@@ -1,31 +1,32 @@
-import { useMutation } from "@tanstack/react-query"
-import { AxiosError } from "axios"
-import { Field, Form, Formik } from "formik"
-import { NavLink, useNavigate } from "react-router-dom"
-import { toFormikValidationSchema } from "zod-formik-adapter"
-import { login } from "../services/auth.service"
-import { User, UserLoginDTO } from "../shared/data-access/api/models/user"
-import { useAuthStore } from "../shared/data-access/store/auth.store"
-import { LoginValidationSchema } from "../shared/data-access/zod-schema"
-import { ErrorList } from "../shared/ui/error-list"
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { Field, Form, Formik } from "formik";
+import { NavLink, useNavigate } from "react-router-dom";
+import { toFormikValidationSchema } from "zod-formik-adapter";
+import { login } from "../services/auth.service";
+import { User, UserLoginDTO } from "../shared/data-access/api/models/user";
+import { useAuthStore } from "../shared/data-access/store/auth.store";
+import { LoginValidationSchema } from "../shared/data-access/zod-schema";
+import { Error } from "../shared/ui/error";
 
 const initialValues: UserLoginDTO = {
   email: "",
   password: "",
-}
+};
 
 export const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const useAuth = useAuthStore();
 
   const { mutate, isPending, isError, error } = useMutation({
     mutationFn: login,
     onSuccess: (data: { user: User }) => {
-      useAuth.setAuthState(data.user, true)
-      navigate("/")
-    }
-  })
+      useAuth.setAuthState(data.user, true);
+      navigate("/");
+    },
+  });
 
+  console.log(error as AxiosError);
   return (
     <div className="auth-page">
       <div className="container page">
@@ -36,32 +37,50 @@ export const Login = () => {
               <NavLink to="/register">Need an account?</NavLink>
             </p>
 
-            {isError && <ErrorList errors={((error as AxiosError).response?.data as any)?.errors} />}
+            {isError && <Error error={(error as AxiosError).message} />}
 
             <Formik
               initialValues={initialValues}
               validationSchema={toFormikValidationSchema(LoginValidationSchema)}
               onSubmit={(user) => mutate({ user: user })}
             >
-              {
-                ({ errors, touched }) => (
-                  <Form>
-                    <fieldset className="form-group" disabled={isPending}>
-                      <Field name="email" className="form-control form-control-lg" type="email" placeholder="Email" />
-                      {errors.email && touched.email ? <span>{errors.email}</span> : null}
-                    </fieldset>
-                    <fieldset className="form-group" disabled={isPending}>
-                      <Field name="password" className="form-control form-control-lg" type="password" placeholder="Password" />
-                      {errors.password && touched.password ? <span>{errors.password}</span> : null}
-                    </fieldset>
-                    <button className="btn btn-lg btn-primary pull-xs-right" type="submit" disabled={isPending}>Sign in</button>
-                  </Form>
-                )
-              }
+              {({ errors, touched }) => (
+                <Form>
+                  <fieldset className="form-group" disabled={isPending}>
+                    <Field
+                      name="email"
+                      className="form-control form-control-lg"
+                      type="email"
+                      placeholder="Email"
+                    />
+                    {errors.email && touched.email ? (
+                      <span>{errors.email}</span>
+                    ) : null}
+                  </fieldset>
+                  <fieldset className="form-group" disabled={isPending}>
+                    <Field
+                      name="password"
+                      className="form-control form-control-lg"
+                      type="password"
+                      placeholder="Password"
+                    />
+                    {errors.password && touched.password ? (
+                      <span>{errors.password}</span>
+                    ) : null}
+                  </fieldset>
+                  <button
+                    className="btn btn-lg btn-primary pull-xs-right"
+                    type="submit"
+                    disabled={isPending}
+                  >
+                    Sign in
+                  </button>
+                </Form>
+              )}
             </Formik>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
---
+++ blog-frontend/src/shared/ui/error.tsx
@@ -0,0 +1,3 @@
+export const Error = ({ error }: { error: string }) => {
+  return <p className="error-messages">{error}</p>;
+};
